73a7665db9ccc851f3651d7a49262c7065ae48ad
Prakshal Pro <prakshal@Prakshals-MacBook-Pro.local>
2021-09-03 00:01:42 -0400

    Added corsheaders and rest_framework to backend

--- zeal_backend/zeal_backend/settings.py
+++ zeal_backend/zeal_backend/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +49,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000'
 ]
 
 ROOT_URLCONF = 'zeal_backend.urls'

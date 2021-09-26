9a994f43b7d8bbcd848090a4a3a30bf870da198a
Prakshal Jain <61107289+Prakshal-Jain@users.noreply.github.com>
2021-09-26 17:29:46 -0400

    Revert "(ZenHub Task: Code landing page#21): Coded the landing page for our website which is the homepage in React"

--- zeal_frontend/src/index.js
+++ zeal_frontend/src/index.js
@@ -1,26 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-// Homepage routes all the pages.
-import Homepage from "./pages/Homepage";
-import ScrollToTop from "./components/ScrollToTop";
-import { HashRouter } from "react-router-dom";
-
-// vendor styles
-import "@fortawesome/fontawesome-free/css/all.css";
-import "react-datetime/css/react-datetime.css";
-
-// core styles
-import "./scss/volt.scss";
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <HashRouter>
-    <ScrollToTop />
-    <Homepage />
-  </HashRouter>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();

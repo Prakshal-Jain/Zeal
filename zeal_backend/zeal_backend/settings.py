da441ac8d15a04d31411bb1cb165845bee9c206a
tommyxchow <tommyxchow@gmail.com>
2021-10-16 15:52:35 -0400

    Minor backend linting fixes #37

--- zeal_backend/zeal_backend/settings.py
+++ zeal_backend/zeal_backend/settings.py
@@ -47,8 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
-    'users'
-    
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -98,7 +97,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'Jainprakshal@22',
+        'PASSWORD': os.getenv('DATABASE_PASSWORD', 'postgres'),
         'HOST': 'localhost',
         'PORT': 5432,
     }

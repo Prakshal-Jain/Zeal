d1f20f438177b2ddcf824362240c02b4bae1f612
aleena-sheikh <aleenabsheikh@gmail.com>
2021-10-18 02:00:44 -0400

    updated serializer #38

--- zeal_backend/users/serializers.py
+++ zeal_backend/users/serializers.py
@@ -1,19 +1,20 @@
 from rest_framework import serializers
 from .models import User
 
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id','name','email','password','username']
+        fields = ['id', 'name', 'email', 'password', 'username', 'profile_pic']
 
-        #write extra properties for fields here
+        # write extra properties for fields here
         extra_kwargs = {
-            'password':{'write_only':True}
+            'password': {'write_only': True}
         }
 
     def create(self, validated_data):
-        #don't return password on post requests
-        password = validated_data.pop('password',None)
+        # don't return password on post requests
+        password = validated_data.pop('password', None)
         instance = self.Meta.model(**validated_data)
         if password is not None:
             instance.set_password(password)

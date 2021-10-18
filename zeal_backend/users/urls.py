65a0e6c8f88333c33f715a70b39e58def3f6cf79
antmar17 <47531838+antmar17@users.noreply.github.com>
2021-10-18 15:46:22 -0400

    Coded first draft of Forgot password backend #29
    Used inbuilt django  api to reset password

--- zeal_backend/users/urls.py
+++ zeal_backend/users/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
 from .views import LoginView, LogoutView, RegisterView,UserView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register', RegisterView.as_view()),
     path('login', LoginView.as_view()),
     path('user',UserView.as_view()),
-    path('logout',LogoutView.as_view())
+    path('logout',LogoutView.as_view()),
+
+    
+    path('reset_password/',auth_views.PasswordResetView.as_view(),name = "reset_password"),
+    path('reset_password_sent/',auth_views.PasswordResetDoneView.as_view(),name = "password_reset_done"),
+    path('reset_password/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(),name = "password_reset_confirm"),
+    path('reset_password_complete/',auth_views.PasswordResetCompleteView.as_view(),name = "password_reset_complete")
+    
 ]
--- zeal_backend/zeal_backend/settings.py
+++ zeal_backend/zeal_backend/settings.py
@@ -162,3 +162,14 @@ CORS_ALLOW_CREDENTIALS = True
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
+
+
+
+
+#SMTP Configuration (for sending email for password reset)
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST= 'smtp.gmail.com'
+EMAIL_PORT= '587'
+EMAIL_USE_TLS= True
+EMAIL_HOST_USER= os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')

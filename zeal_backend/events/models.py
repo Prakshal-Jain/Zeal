f88df6048632e4d89b21d0a3d7da7a4370e84c8d
antmar17 <47531838+antmar17@users.noreply.github.com>
2021-11-01 15:26:37 -0400

    added some fields to Events model #66
    Added many to many model with existing user model

--- zeal_backend/events/models.py
+++ zeal_backend/events/models.py
@@ -1,11 +1,32 @@
 from django.db import models
+from users.models import User
+from django.utils.crypto import get_random_string
 
 # TODO CHANGE HOSTED BY TO A USER
 # TODO add list of users attending
 
 # Create your models here.
 class Event(models.Model):
-    name = models.CharField(max_length=255)
-    HostedBy = models.CharField(max_length=255, unique = True)
-    # date = models.DateTimeField()
+    def generateUniqueCode():
+        unique = get_random_string(10)
+        while(len(Event.objects.filter(code=unique).all()) > 0):
+            unique = get_random_string(10)
+        return unique
+    
+    name = models.CharField(max_length=255, unique = True)                                  
+    description = models.CharField(max_length=10000, blank=True)                           
+    owner = models.CharField(max_length=255, unique = True)                                
+    dateCreated = models.DateTimeField(auto_now_add=True, null=True)
+    startDate = models.DateTimeField(blank=False)                   
+    endDate= models.DateTimeField(blank=False)
+    email = models.EmailField(blank=False)
+    participants = models.ManyToManyField(User)
+    code = models.CharField(unique=True, max_length = 10, null=True, default=generateUniqueCode)
+
+
+    
+    # TODO implement later when path is clear
+    #logo = models.ImageField(upload_to ='event_logos/', default='event_logos/default_event.png', blank=True, null=True)
+
+
     

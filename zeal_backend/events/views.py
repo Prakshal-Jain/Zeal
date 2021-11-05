c36c96d745587dfd6e53cccbbab68ba41b9f077b
aleena-sheikh <aleenabsheikh@gmail.com>
2021-11-05 05:30:17 -0400

    added simple views for hosting event, viewing event as org and participant, no auth #66

--- zeal_backend/events/views.py
+++ zeal_backend/events/views.py
@@ -4,29 +4,44 @@ from django.http import HttpResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, viewsets, permissions, exceptions
-
+from models import EventTeamModel, OrganizerEventModel
 
 from zeal_backend.events import serializer
 from .serializer import OrganizerEventSerializer, EventTeamSerializer
 
 # Create your views here.
 
+
 class OrganizerEventView(APIView):
-    # must be host?
+
+    serializer_class = OrganizerEventSerializer
+
+    def get_queryset(self):
+        return OrganizerEventModel.objects.all()
+
+    def get(self, request):
+        details = self.get_queryset()
+        serializer = OrganizerEventSerializer(details, many=True)
+
+        return Response(serializer.data)
 
     def post(self, request):
+
         serializer = OrganizerEventSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
+
         return Response(serializer.data)
 
-    
+
 class EventTeamView(APIView):
-    
-    def post(self, request):
-            serializer = EventTeamSerializer(data=request.data)
-            serializer.is_valid(raise_exception=True)
-            serializer.save()
-            return Response(serializer.data)
+    serializer_class = EventTeamSerializer
 
+    def get_queryset(self):
+        return EventTeamModel.objects.all()
 
+    def get(self, request):
+        details = self.get_queryset()
+        serializer = EventTeamSerializer(details, many=True)
+
+        return Response(serializer.data)

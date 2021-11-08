bf904dd961f8d7e0e30b71cd13ce5b2e9c3080c4
tommyxchow <tommyxchow@gmail.com>
2021-11-07 19:03:34 -0500

    added sidebar to profile page #63

--- zeal_frontend/src/Router.js
+++ zeal_frontend/src/Router.js
@@ -8,31 +8,45 @@ import SignIn from "./pages/SignIn";
 import Profile from "./pages/Profile";
 import { Routes } from "./routes";
 import ForgotPassword from "./pages/ForgotPassword";
-import Contact from "./pages/ContactForm"
-import Event from "./pages/Events"
-import { RouteWithSidebar } from "./components/RouteComponents"
+import Contact from "./pages/ContactForm";
+import Event from "./pages/Events";
+import { RouteWithSidebar } from "./components/RouteComponents";
+import EventDetails from "./pages/EventDetails";
 
 // Required in index.js to provide routing.
 class Router extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-        }
-    }
-
-    render() {
-        return (
-            <Switch>
-                <Route exact path={Routes.Homepage.path}><Homepage /></Route>
-                <Route exact path={Routes.SignUp.path}><SignUp /></Route>
-                <Route exact path={Routes.SignIn.path}><SignIn /></Route>
-                <Route exact path={Routes.ForgotPassword.path}><ForgotPassword /></Route>
-                <Route exact path={Routes.Contact.path}><Contact /></Route>
-                <Route exact path={Routes.Profile.path}><Profile /></Route>
-                <RouteWithSidebar exact path={Routes.Events.path} component={Event} />
-            </Switch>
-        )
-    }
+  render() {
+    return (
+      <Switch>
+        <Route exact path={Routes.Homepage.path}>
+          <Homepage />
+        </Route>
+        <Route exact path={Routes.SignUp.path}>
+          <SignUp />
+        </Route>
+        <Route exact path={Routes.SignIn.path}>
+          <SignIn />
+        </Route>
+        <Route exact path={Routes.ForgotPassword.path}>
+          <ForgotPassword />
+        </Route>
+        <Route exact path={Routes.Contact.path}>
+          <Contact />
+        </Route>
+        <RouteWithSidebar
+          exact
+          path={Routes.Profile.path}
+          component={Profile}
+        />
+        <RouteWithSidebar exact path={Routes.Events.path} component={Event} />
+        <RouteWithSidebar
+          exact
+          path={Routes.EventDetails.path}
+          component={EventDetails}
+        />
+      </Switch>
+    );
+  }
 }
 
 export default Router;
--- zeal_frontend/src/routes.js
+++ zeal_frontend/src/routes.js
@@ -1,10 +1,11 @@
 export const Routes = {
-    // Pages
-    Homepage: {path: "/"},
-    SignUp: {path: "/signup"},
-    SignIn: {path: "/signin"},
-    Profile: {path: "/profile"},
-    ForgotPassword: {path: "/forgot_password"},
-    Contact: {path: "/contact"},
-    Events: {path: "/events"},
+  // Pages
+  Homepage: { path: "/" },
+  SignUp: { path: "/signup" },
+  SignIn: { path: "/signin" },
+  Profile: { path: "/profile" },
+  ForgotPassword: { path: "/forgot_password" },
+  Contact: { path: "/contact" },
+  Events: { path: "/events" },
+  EventDetails: { path: "/event-details" },
 };

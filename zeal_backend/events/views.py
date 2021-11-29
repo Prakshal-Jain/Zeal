753315d68ccc51cd2f8bcb344cb8ce8074ed267e
aleena-sheikh <aleenabsheikh@gmail.com>
2021-11-29 14:16:22 -0500

    #92 minor error

--- zeal_backend/events/views.py
+++ zeal_backend/events/views.py
@@ -127,10 +127,11 @@ class OrganizerParticipantsListView(viewsets.ModelViewSet):
         user = User.objects.get(id=payload["id"])
         # event_id = self.request.query_params.get("event_id")
         data = self.request.data
-        id = data["id"]
-        
+        id = self.request.query_params.get("event_id")
+
         event = OrganizerEventModel.objects.filter(id=id)
         return event
+
 
 class ParticipantEventLeaveView(viewsets.ModelViewSet):
 
